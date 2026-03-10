ecb1a88fe24b1e94e8af86e04b49c6436683e42d
BVAndy259 <1613949@senati.pe>
2026-03-10 13:16:19 -0500

    funcionalidad de datos}

--- app/src/main/java/com/lordkratos/gestion501/DashboardActivity.java
+++ app/src/main/java/com/lordkratos/gestion501/DashboardActivity.java
@@ -4,21 +4,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.lordkratos.gestion501.empresa.EmpresaActivity;
+import com.lordkratos.gestion501.favoritos.FavoritosActivity;
+import com.lordkratos.gestion501.gastos.GastosActivity;
+import com.lordkratos.gestion501.listaTareas.ListaTareasActivity;
+import com.lordkratos.gestion501.misDatos.MisDatosActivity;
+import com.lordkratos.gestion501.tareas.TareasActivity;
 
 public class DashboardActivity extends AppCompatActivity {
-    private Button btnCerrarSesion;
+    private Button btnCerrarSesion, btnDesarrollador;
+    private CardView cvEmpresa, cvGastos, cvTareas, cvListaTareas, cvFavoritos, cvMisDatos;
+    private TextView tvNombreApellido, tvCodigoU;
     private FirebaseAuth firebaseAuth;
     private FirebaseUser firebaseUser;
+    private DatabaseReference Usuarios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +48,76 @@ public class DashboardActivity extends AppCompatActivity {
             return insets;
         });
 
+        cvEmpresa = findViewById(R.id.cvEmpresa);
+        cvGastos = findViewById(R.id.cvGastos);
+        cvTareas = findViewById(R.id.cvTareas);
+        cvListaTareas = findViewById(R.id.cvListaTareas);
+        cvFavoritos = findViewById(R.id.cvFavoritos);
+        cvMisDatos = findViewById(R.id.cvMisDatos);
+
+        tvNombreApellido = findViewById(R.id.tvNombreApellido);
+        tvCodigoU = findViewById(R.id.tvCodigoU);
+
         btnCerrarSesion = findViewById(R.id.btnCerrarSesion);
+        btnDesarrollador = findViewById(R.id.btnDesarrollador);
+
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseUser = firebaseAuth.getCurrentUser();
+
+        Usuarios = FirebaseDatabase.getInstance().getReference("Usuarios");
 
         btnCerrarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 cerrarSesión();
+            }
+        });
+
+        cvEmpresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DashboardActivity.this, "Esto es Empresa", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(DashboardActivity.this, EmpresaActivity.class));
+            }
+        });
+
+        cvGastos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DashboardActivity.this, "Esto es Gastos", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(DashboardActivity.this, GastosActivity.class));
+            }
+        });
+
+        cvTareas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DashboardActivity.this, "Esto es Tareas", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(DashboardActivity.this, TareasActivity.class));
+            }
+        });
+
+        cvListaTareas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DashboardActivity.this, "Esto es Lista de Tareas", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(DashboardActivity.this, ListaTareasActivity.class));
+            }
+        });
+
+        cvFavoritos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DashboardActivity.this, "Esto es Favoritos", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(DashboardActivity.this, FavoritosActivity.class));
+            }
+        });
+
+        cvMisDatos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DashboardActivity.this, "Esto es Misa Datos", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(DashboardActivity.this, MisDatosActivity.class));
             }
         });
     }
@@ -48,5 +127,40 @@ public class DashboardActivity extends AppCompatActivity {
         startActivity(new Intent(DashboardActivity.this, MainActivity.class));
         Toast.makeText(this, "Cerraste sesión exitosamente", Toast.LENGTH_SHORT).show();
         finish();
+    }
+
+    @Override
+    protected void onStart() {
+        comprobarSesion();
+        super.onStart();
+    }
+
+    private void comprobarSesion() {
+        if (firebaseUser != null) {
+            cargarDatos();
+        } else {
+            startActivity(new Intent(DashboardActivity.this, MainActivity.class));
+            finish();
+        }
+    }
+
+    private void cargarDatos() {
+        Usuarios.child(firebaseAuth.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    String uid = "" + snapshot.child("uid").getValue();
+                    String nombre = "" + snapshot.child("nombre").getValue();
+                    String apellido = "" + snapshot.child("apellido").getValue();
+                    tvNombreApellido.setText(nombre + " " + apellido);
+                    tvCodigoU.setText(uid);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/lordkratos/gestion501/misDatos/MisDatosActivity.java
+++ app/src/main/java/com/lordkratos/gestion501/misDatos/MisDatosActivity.java
@@ -1,16 +1,30 @@
 package com.lordkratos.gestion501.misDatos;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.lordkratos.gestion501.MainActivity;
 import com.lordkratos.gestion501.R;
 
 public class MisDatosActivity extends AppCompatActivity {
+    private TextView tvCorreo, tvCodigo;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+    private DatabaseReference Usuarios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +35,47 @@ public class MisDatosActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        tvCodigo = findViewById(R.id.tvCodigoMD);
+        tvCorreo = findViewById(R.id.tvCorreoMD);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        Usuarios = com.google.firebase.database.FirebaseDatabase.getInstance().getReference("Usuarios");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        comprobarSesion();
+    }
+
+    private void comprobarSesion() {
+        if (firebaseUser != null) {
+            cargarDatos();
+        } else {
+            startActivity(new Intent(MisDatosActivity.this, MainActivity.class));
+            finish();
+        }
+    }
+
+    private void cargarDatos() {
+        Usuarios.child(firebaseAuth.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    String uid = "" + snapshot.child("uid").getValue();
+                    String correo = "" + snapshot.child("correo").getValue();
+                    tvCorreo.setText(correo);
+                    tvCodigo.setText(uid);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
         });
     }
 }
